8f407b7853eac1220199e6556f503a0f463415b5
Donald Chow <donald7290@gmail.com>
2023-08-08 00:30:55 +0900

    migrate model resort

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_07_152310) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_07_152759) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "resorts", force: :cascade do |t|
+    t.string "name"
+    t.string "prefecture"
+    t.string "town"
+    t.string "address"
+    t.integer "trial_length"
+    t.integer "longest_trial"
+    t.integer "skiable_terrain"
+    t.integer "number_of_trails"
+    t.integer "vertical_drop"
+    t.integer "lift"
+    t.integer "gondola"
+    t.integer "base_altitude"
+    t.integer "highest_altitude"
+    t.integer "steepest_gradient"
+    t.integer "difficulty_green"
+    t.integer "difficulty_red"
+    t.integer "difficulty_black"
+    t.boolean "terrain_park"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_resorts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +52,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_07_152310) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "resorts", "users"
 end
