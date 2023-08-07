7d343846fb888b88f5101a1283fdeffdf2928dc9
Donald Chow <donald7290@gmail.com>
2023-08-08 01:46:04 +0900

    endpoint destroy

--- app/controllers/api/v1/resorts_controller.rb
+++ app/controllers/api/v1/resorts_controller.rb
@@ -28,6 +28,11 @@ class Api::V1::ResortsController < Api::V1::BaseController
     end
   end
 
+  def destroy
+    @resort.destroy
+    head :no_content
+  end
+
   private
 
   def set_resort
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :resorts, only: %i[index show update create]
+      resources :resorts, only: %i[index show update create destroy]
     end
   end
 end
