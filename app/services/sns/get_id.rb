c459ec002a101353edd1ea9e924353b391862e3c
Donald Chow <donald7290@gmail.com>
2024-03-11 20:59:35 +0900

    get all id scraper for sns

---
+++ app/services/sns/get_id.rb
@@ -0,0 +1,39 @@
+require 'nokogiri'
+require 'open-uri'
+
+class Sns::GetId < ApplicationService
+  def initialize
+    @url = "https://surfsnow.jp/search/list/spl_area01.php?areacdl="
+    @range = (1..7)
+  end
+
+  def call
+    list = []
+    @range.each do |n|
+      base_url = @url + n.to_s
+      doc = Nokogiri::HTML(URI.open(base_url))
+      list += scrap(doc)
+
+      # check if there are multiple pages
+      nav_bar = doc.search('.page_nav a')[-2]
+
+      # return list of ids if there is only one page
+      return list unless nav_bar
+
+      # repeat scrapper if there are more than 1 page
+      last_page = nav_bar.values.first.gsub(/\D/, '').to_i
+      (2..last_page).each do |page|
+        url = base_url + "&page=#{page}"
+        doc = Nokogiri::HTML(URI.open(url))
+        list += scrap(doc)
+      end
+    end
+    list
+  end
+
+  private
+
+  def scrap(doc)
+    doc.search('.list_result h2 a').to_a.map{ |x| x.attribute_nodes[0].value[%r{/htm/(.*?)s\.htm}, 1] }
+  end
+end
