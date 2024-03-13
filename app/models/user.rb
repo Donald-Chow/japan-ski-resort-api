9d2bfe6774a1b8d9d7e8dbe32683dc6d76388c49
Donald Chow <donald7290@gmail.com>
2024-03-13 12:14:24 +0900

    changes for heroku one-db production

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  if ENV.fetch("RAILS_ENV") && ENV.fetch("RAILS_ENV") == 'production'
+    self.table_name = 'jp_ski_user'
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   acts_as_token_authenticatable
